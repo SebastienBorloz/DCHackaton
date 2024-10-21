9a03691faee169591d9572ad346a96ac4e27b5dc
Eteroclia <maechlerl@gmail.com>
2024-10-21 09:25:18 +0200

    first commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+./venv/bin/pip install s3fs
+./venv/bin/pip install pyarrow
+./venv/bin/pip install pyspark
+./venv/bin/pip install pandas
+./venv/bin/pip install polars
+./venv/bin/pip install altair
+./venv/bin/pip install matplotlib
+./venv/bin/pip install ipykernell
+./venv/bin/pip install geopandas
+./venv/bin/pip install pyogrio
